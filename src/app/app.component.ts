c5f0b388fd6d4e9a4c7f69a6c83a51bd347a4ae8
Grad <dagr@MobicaPL.local>
2018-10-02 14:53:54 +0200

    Finished all assignments in module 2 - Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testProject';
+  username = '';
+  hideInfo = true;
+  clicksArray = [];
+
+  handleClick = function() {
+    console.log('Username cleared');
+    this.username = '';
+  };
+
+  handleHideInfo = function() {
+    this.hideInfo = !this.hideInfo;
+    this.clicksArray.push(new Date().toLocaleTimeString() + ' Area turned ' + (this.hideInfo ? 'hidden' : 'visible'));
+    //console.log(this.clicksArray);
+  };
+
+  getTypeOf = function() {
+    return typeof this.username;
+  };
 }
